c781211b871af2cd9db658f0ba7941cc692c98b1
Truebamateo <32852493+Truebamateo@users.noreply.github.com>
2017-12-05 20:00:46 -0300

    BETA 1.0
    - Eliminado completamente el cuadro de comandos.
    + Añadida interfaz de botones.
    * Varias mejoras y bugs arreglados.

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Superhex.io Scr1pt
 // @namespace    SuperhexIoScr1pt
-// @version      0.1.4
+// @version      BETA_1.0
 // @homepageURL  https://github.com/Truebamateo/Superhex.io-Scr1pt
 // @icon         http://superhex.io/img/fav_icon_1.png
 // @description  Script para Superhex.io
@@ -26,43 +26,92 @@ document.onkeydown = function(e){
     }
 };
 
-commandsTBM = function(cmd){
-    cmd = window.prompt("Superhex.io Scr1pt por Truebamateo - Insertar comando: (Para ver la lista de comandos insertar Comandos)");
-    if (cmd == "Comandos" || cmd == "comandos" || cmd == "COMANDOS")
+goGitHub = function()
+{
+  window.open("https://github.com/Truebamateo/Superhex.io-Scr1pt");
+};
+
+changeQ = function()
+{
+    var QualityPrompt = window.prompt("Calidad del juego:\n0.5: Low (Baja)\n0.75: Medium (Media)\n1: High (Alta)");
+    QualityPrompt = Number(QualityPrompt);
+    if(QualityPrompt > 2.7)alert("ADVERTENCIA: Establecer la calidad mayor a 2.7 puede causar problemas");
+    if(QualityPrompt < 0.1 && QualityPrompt > 0)alert("ADVERTENCIA: Establecer la calidad menor a 0.1 puede causar problemas");
+    if(QualityPrompt.toString() == "NaN")
     {
-        alert(" Comandos:\nComandos: Muestra la lista de comandos.\nCalidad: Establece la calidad del juego.\nParty: Crea una party para jugar con tus amig@s.\nSkin: Selecciona la skin que quieres utilizar.\nTeclas: Muestra la lista de las teclas con funciones.\nTexto1: Cambia el texto del botón Play.");
-    } else if(cmd == "Calidad" || cmd == "calidad" || cmd == "CALIDAD")
-    {
-        var QualityPrompt = window.prompt("Calidad del juego: (Máximo 2.8)");
-        QualityPrompt = Number(QualityPrompt);
+        alert("Calidad inválida. Solo puedes insertar números.\nEjemplo: 1.2");
+    } else if(QualityPrompt === 0) {
+        alert("Calidad no cambiada.");
+    } else {
         superhex.setQuality(QualityPrompt);
-    } else if(cmd == "Party" || cmd == "party" || cmd == "PARTY")
+        alert("Calidad cambiada a: " + QualityPrompt);
+    }
+};
+
+changeS = function()
+{
+    var SkinPrompt = window.prompt("ID de la Skin: (Si te abre una página, cierrala)");
+    SkinPrompt = Number(SkinPrompt);
+    if(SkinPrompt.toString() == "NaN")
     {
-      superhex.createParty();
-    } else if(cmd == "Skin" || cmd == "skin" || cmd == "SKIN")
-    {
-        var SkinPrompt = window.prompt("ID de la Skin: (Si te abre una página, cierrala)");
-        SkinPrompt = Number(SkinPrompt);
+        alert("ID de la skin inválida. Solo puedes insertar números.");
+    } else if(SkinPrompt === 0) {
+        alert("Skin no cambiada.");
+    } else {
         superhex.selectSkin(SkinPrompt);
-    } else if(cmd == "Teclas" || cmd == "teclas" || cmd=="TECLAS")
+        alert("Skin cambiada a: " + SkinPrompt);
+    }
+};
+
+teclasAc = function()
+{
+    alert("Teclas con funciones:\n+ (*) : Inicia el juego sin el anuncio.");
+};
+
+changeT1 = function()
+{
+  var Text1Prompt = window.prompt("Texto del botón Play:");
+  if(Text1Prompt === null)
     {
-        alert(" Teclas con funciones:\n+ (*) : Inicia el juego sin el anuncio.");
-    } else if(cmd == "Texto1" || cmd == "texto1" || cmd == "TEXTO1")
-    {
-        var Text1Prompt = window.prompt("Texto del botón Play:");
+        alert("Texto del botón Play no cambiado.");
+    } else {
         document.getElementById("button-play-text").innerText = Text1Prompt;
         window.localStorage.setItem("Text1TBM", Text1Prompt);
+        alert("Texto del botón Play cambiado a: " + Text1Prompt);
     }
-        };
+};
 
-var btn = document.createElement("IMG");
-btn.setAttribute("src", "http://www.healthmarketsinc.com/wp-content/uploads/2015/03/hamburger-menu.png");
-btn.setAttribute("style", "position: fixed; top: 100px;");
-btn.setAttribute("onclick", "commandsTBM();");
+var scrText1 = document.createElement("h2");
+scrText1.setAttribute("style", "color: white; position: fixed; top: 50px; left: 30px;");
+scrText1.innerText = "Superhex.io Scr1pt vBETA 1.0";
+document.getElementById("homepage").appendChild(scrText1);
+
+var btn = document.createElement("Button");
+btn.setAttribute("style", "position: fixed; top: 110px; left: 30px;");
+btn.innerText = "GitHub";
+btn.setAttribute("onclick", "goGitHub();");
 document.getElementById("homepage").appendChild(btn);
 
-var btn2 = document.createElement("IMG");
-btn2.setAttribute("src", "http://www.healthmarketsinc.com/wp-content/uploads/2015/03/hamburger-menu.png");
-btn2.setAttribute("style", "position: fixed; top: 150px;");
-btn2.setAttribute("onclick", "commandsTBM();");
-document.getElementById("respawn").appendChild(btn2);
+var btn2 = document.createElement("Button");
+btn2.setAttribute("style", "position: fixed; top: 140px; left: 30px;");
+btn2.innerText = "Cambiar calidad";
+btn2.setAttribute("onclick", "changeQ();");
+document.getElementById("homepage").appendChild(btn2);
+
+var btn3 = document.createElement("Button");
+btn3.setAttribute("style", "position: fixed; top: 170px; left: 30px;");
+btn3.innerText = "Cambiar skin (BETA)";
+btn3.setAttribute("onclick", "changeS();");
+document.getElementById("homepage").appendChild(btn3);
+
+var btn4 = document.createElement("Button");
+btn4.setAttribute("style", "position: fixed; top: 200px; left: 30px;");
+btn4.innerText = "Teclas con funciones";
+btn4.setAttribute("onclick", "teclasAc()");
+document.getElementById("homepage").appendChild(btn4);
+
+var btn5 = document.createElement("Button");
+btn5.setAttribute("style", "position: fixed; top: 230px; left: 30px;");
+btn5.innerText = "Texto del botón Play";
+btn5.setAttribute("onclick", "changeT1()");
+document.getElementById("homepage").appendChild(btn5);

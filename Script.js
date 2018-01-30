390992688aad3081d7bee34ee427cbf442752dfa
Truebamateo <32852493+Truebamateo@users.noreply.github.com>
2018-01-30 20:15:02 -0300

    1.2.3
    * Mejorada considerablemente la función para establecer el skin con un ID.
    * Otros cambios y mejoras.

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Superhex.io Scr1pt
 // @namespace    Superhex.io Scr1pt
-// @version      1.2.2
+// @version      1.2.3
 // @homepageURL  https://github.com/Truebamateo/Superhex.io-Scr1pt
 // @icon         http://superhex.io/img/fav_icon_1.png
 // @description  Script para Superhex.io
@@ -14,11 +14,55 @@
 var Text1TBM = localStorage.getItem("Text1TBM");
 var AdsTBM = localStorage.getItem("AdsTBM");
 var adsDeleted = false;
+var skinPag = 1;
+//var currSkin = 0;
 
 window.onload = function ()
 {
   mkGui();
+  var skinRightArrow = document.getElementById("skin-right-arrow");
+  var skinLeftArrow = document.getElementById("skin-left-arrow");
+  skinRightArrow.setAttribute("onclick", "skinChangePage(true, 1)");
+  skinLeftArrow.setAttribute("onclick", "skinChangePage(false, 1)");
 };
+
+skinChangePage = function(next, cantidad)
+{
+  if(!next) {
+    if(cantidad == 1) {
+    superhex.previousSkins();
+    skinPag--;
+  } else if(cantidad == 2) {
+    superhex.previousSkins();
+    superhex.previousSkins();
+    skinPag = skinPag - 2;
+  } else if(cantidad == 3) {
+    superhex.previousSkins();
+    superhex.previousSkins();
+    superhex.previousSkins();
+    skinPag = skinPag - 3;
+  }
+  } else {
+    if(cantidad == 1)  {
+    superhex.nextSkins();
+    skinPag++;
+  } else if(cantidad == 2) {
+    superhex.nextSkins();
+    superhex.nextSkins();
+    skinPag = skinPag + 2;
+  } else if(cantidad == 3) {
+    superhex.nextSkins();
+    superhex.nextSkins();
+    superhex.nextSkins();
+    skinPag = skinPag + 3;
+  }
+  }
+};
+
+/*changeSkin = function(ID)
+{                               COMING SOON - PRÓXIMAMENTE
+
+}; */
 
 removeAds = function(checkBox)
 {
@@ -34,9 +78,11 @@ removeAds = function(checkBox)
     superhex.clickPlay = superhex.aipComplete;
     superhex.clickPlayAgain = superhex.aipComplete;
     if(!adsDeleted) {
-    document.getElementById("div-gpt-ad-1490263292583-0").innerHTML = "Anuncio bloqueado por Superhex.io Scr1pt";
-    document.getElementById("div-gpt-ad-1490263292583-0").setAttribute("id", "adBlockedTBM"); //data-google-query-id
-    document.getElementById("adBlockedTBM").setAttribute("data-google-query-id", "null");
+    var Ad14 = document.getElementById("div-gpt-ad-1490263292583-0");
+    Ad14.innerHTML = "Anuncio bloqueado por Superhex.io Scr1pt";
+    Ad14.setAttribute("id", "adBlockedTBM");
+    Ad14 = document.getElementById("adBlockedTBM");
+    Ad14.setAttribute("data-google-query-id", "null");
     document.getElementById("cdm-zone-01").innerHTML = "Anuncio bloqueado por Superhex.io Scr1pt";
   }
     alert("Anuncios desactivados.");
@@ -45,9 +91,11 @@ removeAds = function(checkBox)
       superhex.clickPlay = superhex.aipComplete;
       superhex.clickPlayAgain = superhex.aipComplete;
       setTimeout(function(){
-        document.getElementById("div-gpt-ad-1490263292583-0").innerHTML = "Anuncio bloqueado por Superhex.io Scr1pt";
-        document.getElementById("div-gpt-ad-1490263292583-0").setAttribute("id", "adBlockedTBM");
-        document.getElementById("adBlockedTBM").setAttribute("data-google-query-id", "null");
+        var Ad14 = document.getElementById("div-gpt-ad-1490263292583-0");
+        Ad14.innerHTML = "Anuncio bloqueado por Superhex.io Scr1pt";
+        Ad14.setAttribute("id", "adBlockedTBM");
+        Ad14 = document.getElementById("adBlockedTBM");
+        Ad14.setAttribute("data-google-query-id", "null");
         document.getElementById("cdm-zone-01").innerHTML = "Anuncio bloqueado por Superhex.io Scr1pt";
       },1350);
    }
@@ -81,16 +129,77 @@ changeQ = function()
 
 changeS = function()
 {
-    var SkinPrompt = window.prompt("ID de la Skin: (Si te abre una página, cierrala)");
+    var SkinPrompt = window.prompt("ID de la Skin:\nNota: Para establecer la skin 0 se debe utilizar 0.1");
     SkinPrompt = Number(SkinPrompt);
+    superhex.nextSkins();
+    superhex.previousSkins();
+    var ChickenS2 = false;
+    var TweetS2 = false;
+    var CowS2 = false;
+    var RedBirdS2 = false;
+    var ElephantS2 = false;
+    if(localStorage.getItem("followClicked")) ChickenS2 = true;
+    if(localStorage.getItem("tweetClicked")) TweetS2 = true;
+    if(localStorage.getItem("likeClicked")) CowS2 = true;
+    if(localStorage.getItem("subscribeClicked")) RedBirdS2 = true;
+    if(localStorage.getItem("shareClicked")) ElephantS2 = true;
     if(SkinPrompt.toString() == "NaN")
     {
-        alert("ID de la skin inválida. Solo puedes insertar números.");
+        alert("ID de la skin no válido. Solo puedes insertar números.");
+    } else if(SkinPrompt == 0.1) {
+        if(skinPag == 2) skinChangePage(false, 1);
+        if(skinPag == 3) skinChangePage(false, 2);
+        if(!ChickenS2) localStorage.setItem("followClicked", 1);
+        superhex.selectSkin(0);
+        if(!ChickenS2) localStorage.removeItem("followClicked");
+        alert("Skin cambiada a: 0");
     } else if(SkinPrompt === 0) {
         alert("Skin no cambiada.");
     } else {
-        superhex.selectSkin(SkinPrompt);
-        alert("Skin cambiada a: " + SkinPrompt);
+        if(SkinPrompt == 1) {
+           if(skinPag == 2) skinChangePage(false, 1);
+           if(skinPag == 3) skinChangePage(false, 2);
+           if(!TweetS2) localStorage.setItem("tweetClicked", 1);
+           superhex.selectSkin(SkinPrompt);
+           alert("Skin cambiada a: " + SkinPrompt);
+           if(!TweetS2) localStorage.removeItem("tweetClicked");
+        } else if(SkinPrompt == 2) {
+           if(skinPag == 2) skinChangePage(false, 1);
+           if(skinPag == 3) skinChangePage(false, 2);
+           if(!CowS2) localStorage.setItem("likeClicked", 1);
+           superhex.selectSkin(SkinPrompt);
+           alert("Skin cambiada a: " + SkinPrompt);
+           if(!CowS2) localStorage.removeItem("likeClicked");
+        } else if(SkinPrompt == 3) {
+           if(skinPag == 2) skinChangePage(false, 1);
+           if(skinPag == 3) skinChangePage(false, 2);
+           if(!RedBirdS2) localStorage.setItem("subscribeClicked", 1);
+           superhex.selectSkin(SkinPrompt);
+           alert("Skin cambiada a: " + SkinPrompt);
+           if(!RedBirdS2) localStorage.removeItem("subscribeClicked");
+        } else if(SkinPrompt == 4) {
+           if(skinPag == 1) skinChangePage(true, 1);
+           if(skinPag == 3) skinChangePage(false, 1);
+           if(!ElephantS2) localStorage.setItem("shareClicked", 1);
+           superhex.selectSkin(SkinPrompt);
+           alert("Skin cambiada a: " + SkinPrompt);
+           if(!ElephantS2) localStorage.removeItem("shareClicked");
+        } else {
+          if(SkinPrompt <= 9) {
+          try {
+           if(SkinPrompt >= 8 && skinPag == 1) skinChangePage(true, 2);
+           if(SkinPrompt >= 8 && skinPag == 2) skinChangePage(true, 1);
+           if(SkinPrompt >= 4 && SkinPrompt < 8 && skinPag == 1) skinChangePage(true, 1);
+           if(SkinPrompt >= 4 && SkinPrompt < 8 && skinPag == 3) skinChangePage(false, 1);
+           superhex.selectSkin(SkinPrompt);
+           alert("Skin cambiada a: " + SkinPrompt);
+          } catch(err) {
+            alert("Se ha producido un error. Asegúrate de insertar un ID válido.");
+          }
+          } else {
+            alert("La skin " + SkinPrompt + " no existe. Introduzca un número menor a 10.");
+          }
+        }
     }
 };
 
@@ -146,7 +255,7 @@ document.getElementById("homepage").appendChild(scrText1);
 
 mkGui = function() {
 
-scrText1.innerText = "Superhex.io Scr1pt v1.2.2";
+scrText1.innerText = "Superhex.io Scr1pt v1.2.3";
 
 var btn = document.createElement("Button");
 btn.setAttribute("style", "position: fixed; top: 110px; left: 30px; height:20px; width:140px;");
@@ -168,7 +277,7 @@ var btn3 = document.createElement("Button");
 btn3.setAttribute("style", "position: fixed; top: 170px; left: 30px; height:20px; width:140px;");
 btn3.setAttribute("class", "green");
 btn3.setAttribute("type", "button");
-btn3.innerText = "Cambiar skin (BETA)";
+btn3.innerText = "Establecer Skin (ID)";
 btn3.setAttribute("onclick", "changeS();");
 document.getElementById("homepage").appendChild(btn3);
 
@@ -191,7 +300,7 @@ if(AdsTBM) Check1.checked = true;
 
 var check1Text = document.createElement("h5");
 check1Text.setAttribute("style", "color: white; position: fixed; top: 210px; left: 50px;");
-check1Text.innerText = "Desactivar anuncios.";
+check1Text.innerText = "Desactivar anuncios";
 document.getElementById("homepage").appendChild(check1Text);
 
 var btn6 = document.createElement("Button");
